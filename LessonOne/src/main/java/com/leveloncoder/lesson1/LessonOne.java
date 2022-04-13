7b2b95a01f627dc09312970da7898f3b83c2f506
LevelOnCoder <81438214+LEVELONCODER@users.noreply.github.com>
2022-04-13 12:37:17 +0500

    Add files via upload

---
+++ LessonOne/src/main/java/com/leveloncoder/lesson1/LessonOne.java
@@ -0,0 +1,26 @@
+package com.leveloncoder.lesson1;
+
+// We create file and see this code
+// Public it's modificator of access
+// class it's class
+// LessonOne it's name of this class
+// if you have public modificator and your file name is for example main
+// you should name your class as your file
+// if class don't ave public modificator like Main
+// You mustn't name class as file
+
+public class LessonOne {
+
+}
+
+class Main {
+
+}
+
+
+// Download intellij idea here
+// https://www.jetbrains.com/idea/download/
+// install idea and open it
+// download jdk
+// choose 16 jdk version
+// create project
